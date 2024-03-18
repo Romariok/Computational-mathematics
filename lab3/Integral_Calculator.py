b6135b8996804900b8ee7eda571cecc1acb4f0f1
Romariok <ronya-ko@yandex.ru>
2024-03-18 10:15:44 +0300

    full

--- lab3/Integral_Calculator.py
+++ lab3/Integral_Calculator.py
@@ -1,7 +1,6 @@
 import math
 
 
-# Define the function options as callable objects.
 def quadratic(x):
     return 3 * x**2 + 2 * x - 5
 
@@ -30,6 +29,7 @@ class IntegralCalculator:
     def __init__(self, a, b, e, func_choice, method_choice):
         self.a = a
         self.b = b
+        self.perm = True
         self.e = e
         self.func = self.choose_function(func_choice)
         self.method = self.choose_method(method_choice)
@@ -38,6 +38,7 @@ class IntegralCalculator:
         if self.func == root:
             if self.a > 1 or self.b > 1 or self.a < -1 or self.b < -1:
                 print("Интеграл расходится")
+                self.perm = False
             elif self.b == 1:
                 print("Интервал интегрирования заходит на точки разрыва - 1")
                 self.b = self.b - 0.00000000001
@@ -45,12 +46,13 @@ class IntegralCalculator:
                 print("Интервал интегрирования заходит на точки разрыва - -1")
                 self.a = self.a + 0.00000000001
         if self.func == hyperbola:
-            if self.a == 0 and self.b > 0:
+            if self.a < 0 and self.b > 0:
+                self.perm = False
+                print("Интеграл расходится")
+            elif self.a == 0 and self.b > 0:
                 self.a = self.a + 0.00000000001
             if self.a < 0 and self.b == 0:
                 self.b = self.b - 0.00000000001
-            else:
-                print("Интеграл расходится")
 
     def choose_function(self, choice):
         match choice:
@@ -81,21 +83,25 @@ class IntegralCalculator:
                 return self.simpson
 
     def calculate_integral(self, n):
-        num = n
-        while True:
-            h = (self.b - self.a) / num
-            if self.method == self.simpson:
-                coefficient = 15
-            else:
-                coefficient = 3
+        if self.perm:
+            num = n
+            while num <= 1000000:
+                h = (self.b - self.a) / num
+                if self.method == self.simpson:
+                    coefficient = 15
+                else:
+                    coefficient = 3
 
-            integral_value = self.method(num, h)
-            error = abs(integral_value - self.method(2 * num, h / 2)) / coefficient
-            if error <= self.e:
-                break
-            num *= 2
+                integral_value = self.method(num, h)
+                error = abs(integral_value - self.method(2 * num, h / 2)) / coefficient
+                if error <= self.e:
+                    break
+                num *= 2
 
-        return integral_value, num, error
+            return integral_value, num, error
+        else:
+            print("Значение данного интеграла подсчитать нельзя!")
+            return 0, 0, 0
 
     def left_rectangles(self, n, h):
         total = 0
--- lab3/main.py
+++ lab3/main.py
@@ -3,6 +3,20 @@ import Integral_Calculator
 
 
 def main():  # -f - прочитать данные из файла
+    ascii_art = [
+        "           88                                              ",
+        "           88                                              ",
+        "           88                                              ",
+        " ,adPPYba, 88  ,adPPYba,  8b      db      d8 8b,dPPYba,   ",
+        'a8"     "" 88 a8"     "8a `8b    d88b    d8\' 88P\'   `"8a  ',
+        "8b         88 8b       d8  `8b  d8'`8b  d8'  88       88  ",
+        '"8a,   ,aa 88 "8a,   ,a8"   `8bd8\'  `8bd8\'   88       88  ',
+        ' `"Ybbd8"\' 88  `"YbbdP"\'      YP      YP     88       88  ',
+    ]
+
+    for line in ascii_art:
+        print(line)
+
     argv = sys.argv[1:]
 
     if len(argv) == 0:
@@ -26,10 +40,10 @@ def main():  # -f - прочитать данные из файла
         case _:
             try:
                 print(
-                    "Доступные уравнения:\n1: 3 * x**2 + 2 * x - 5\n2: exp(x)\n3: cosh(x)\n4: 2 * x**3 + 3 * x**2 - 5 * x + 7\n5: 1 / sqrt(1 - x**2)"
+                    "Доступные уравнения:\n1: 3 * x**2 + 2 * x - 5\n2: exp(x)\n3: cosh(x)\n4: 2 * x**3 + 3 * x**2 - 5 * x + 7\n5: 1 / sqrt(1 - x**2)\n6: 1 / x"
                 )
                 n = int(input("Введите номер уравнения: "))
-                if n < 1 or n > 4:
+                if n < 1 or n > 6:
                     raise KeyboardInterrupt
                 print(
                     "Доступные методы:\n1: Метод прямоугольника(левых)\n2: Метод прямоугольника(правых)\n3: Метод прямоугольника(средних)\n4: Метод Трапеций\n5: Метод Симпсона"
@@ -66,7 +80,9 @@ def main():  # -f - прочитать данные из файла
     calculator = Integral_Calculator.IntegralCalculator(a, b, e, n, m)
     calculator.check_invalid()
     integral, n, error = calculator.calculate_integral(4)
-    print(f"Calculated integral: {integral}\nNumber of intervals: {n}\nError: {error}")
+    print(
+        f"Значение интеграла: {integral}\nКоличество интервалов: {n}\nТочность: {error}"
+    )
 
 
 if __name__ == "__main__":
