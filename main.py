db8d13b0d9871c12a0fe327575381c5984e921a0
Romariok <ronya-ko@yandex.ru>
2024-04-23 23:50:29 +0300

    all requests

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
+import numpy as np
 import lab4.Approximation as Approximation
 import lab5.Interpolation as Int
 
@@ -9,16 +10,103 @@ CORS(app, resources={r"*": {"origins": "http://localhost:5173"}})
 
 
 
+@app.route("/lab5/function", methods=["POST"])
+def solve_interpolation_function():  
+    def fun1(x):
+        return 2*np.sin(x) - 4
+    def fun2(x):
+        return 13 - 5*np.cos(x)
+    try:
+        data = request.get_json()
+        n = data.get("n", 0)
+        a = data.get("a", 0)
+        b = data.get("b", 0)
+        x = []
+        y = []
+        num = data.get("num", 0)
+        if num<1 or num >2:
+            raise ValueError("Error: unknown number of file")
+        
+        h = (b-a)/n
+        x = [a+h*i for i in range(n)]
+        if num ==1:
+            y = [fun1(x[i]) for i in range(n)]
+        else:
+            y = [fun2(x[i]) for i in range(n)]
+        
+        values = []
+        value = data.get("value", "")
+        value = float(value)
+        if len(x) != len(y):
+            raise KeyboardInterrupt
+        print(x)
+        print(y)
+        print(value)
+        calculator = Int.Interpolation(len(x), x, y)
+        values.append(calculator.lagrange(value))
+        values.append(calculator.newton(value))
+        
+        calculator.difference_table()
+        values.append(calculator.gauss(value, h))
+        
+        answer = {"values": values, "defy": calculator.defy, "data_points": [[x[i], y[i]] for i in range(len(x))],}
+        return jsonify(answer)
+    except KeyboardInterrupt:
+        return jsonify(error=str("Поступили неправильные данные x и y")), 400
+    except ValueError as e:
+        return jsonify(error=str(e)), 400    
+
+
+
+@app.route("/lab5/file", methods=["POST"])
+def solve_interpolation_file():  
+    try:
+        data = request.get_json()
+        x = []
+        y = []
+        num = data.get("num", 0)
+        if num<1 or num >3:
+            raise ValueError("Error: unknown number of file")
+        
+        f = open(f"lab5/input{num}.txt", "r", encoding="utf-8")
+        x_values = f.readline().replace(",", ".").strip().split()
+        x.extend(map(float, x_values))
+        y_values = f.readline().replace(",", ".").strip().split()
+        y.extend(map(float, y_values))
+        
+        values = []
+        value = data.get("value", "")
+        value = float(value)
+        if len(x) != len(y):
+            raise KeyboardInterrupt
+        print(x)
+        print(y)
+        print(value)
+        calculator = Int.Interpolation(len(x), x, y)
+        values.append(calculator.lagrange(value))
+        values.append(calculator.newton(value))
+        
+        h = x[1] - x[0]
+        calculator.difference_table()
+        values.append(calculator.gauss(value, h))
+        
+        answer = {"values": values, "defy": calculator.defy, "data_points": [[x[i], y[i]] for i in range(len(x))],}
+        return jsonify(answer)
+    except KeyboardInterrupt:
+        return jsonify(error=str("Поступили неправильные данные x и y")), 400
+    except ValueError as e:
+        return jsonify(error=str(e)), 400    
+    except:
+        return jsonify(
+            error="Не получилось загрузить данные из файла!\nПроверьте валидность данных"
+        ), 400    
+        
 @app.route("/lab5/app", methods=["POST"])
 def solve_interpolation():
     try:
-        # 0.15 0.2 0.33 0.47 0.62
-        # 1.25 2.38 3.79 5.44 7.14
-        # 0.35
         data = request.get_json()
         x = data.get("x", [])
         y = data.get("y", [])
-        differences = []
         values = []
         value = data.get("value", "")
         value = float(value)
@@ -32,24 +120,19 @@ def solve_interpolation():
         
         
         values.append(calculator.lagrange(value))
-        differences.append([abs(y[i]- calculator.lagrange(x[i])) for i in range(len(x))])
         
         values.append(calculator.newton(value))
-        differences.append([abs(y[i]- calculator.newton(x[i])) for i in range(len(x))])
         
         h = x[1] - x[0]
         calculator.difference_table()
         values.append(calculator.gauss(value, h))
-        differences.append([abs(y[i]- calculator.gauss(x[i], h)) for i in range(len(x))])
         
-        answer = {"values": values, "differences": differences, "defy": calculator.defy}
+        answer = {"values": values, "defy": calculator.defy}
         return jsonify(answer)
     except KeyboardInterrupt:
         return jsonify(error=str("Поступили неправильные данные x и y")), 400
     except ValueError as e:
         return jsonify(error=str(e)), 400
-
-
 
 @app.route("/lab4/app", methods=["POST"])
 def solve_approximation():
