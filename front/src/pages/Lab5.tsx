1edea508e6f3fa82a6b791e7a745da8d3469860b
Romariok <ronya-ko@yandex.ru>
2024-04-23 23:50:51 +0300

    good form

--- front/src/pages/Lab5.tsx
+++ front/src/pages/Lab5.tsx
@@ -18,9 +18,15 @@ import '/src/assets/css/main_page.css'
 function Lab5(): JSX.Element {
    const [isOpen, setIsOpen] = useState<boolean>(false);
    const [success, setSuccess] = useState<boolean>(false);
-   const [successText, setSuccessText] = useState<string>();
+   const [successText, setSuccessText] = useState<number>();
    const [X, setX] = useState<string>("");
    const [Y, setY] = useState<string>("");
+   const [N, setN] = useState<number>(0);
+   const [A, setA] = useState<number>(0);
+   const [B, setB] = useState<number>(0);
+   const [VALUE, setValue] = useState<number>(0);
+   const [numFile, setNumFile] = useState<number>(0);
+   const [numFunc, setNumFunc] = useState<number>(0);
    const [solution, setSolution] = useState<any>("");
    const [error, setError] = useState<boolean>(false);
    const [errorText, setErrorText] = useState<string>();
@@ -38,30 +44,33 @@ function Lab5(): JSX.Element {
    };
 
 
-   const handleSubmitFile = async (e: React.FormEvent<HTMLFormElement>) => {
+   const handleSubmitFunc = async (e: React.FormEvent<HTMLFormElement>) => {
       playSound();
-      console.log("file")
+      console.log("func")
       e.preventDefault();
 
       setSolution({
          err: "",
-         function: "",
-         coefficients: [],
-         differences: [],
-         epsilon_values: [],
-         pearson_correlation: 0,
-         phi_values: [],
          data_points: [],
-         standard_deviation: 0,
-         func: "",
+         values: [],
+         defy: []
       });
 
-
+      const n = N
+      const a = A
+      const b = B
+      const num = numFunc
+      const value = VALUE
+      console.log(num, value)
       try {
          const response = await fetch(
-            "http://127.0.0.1:5000/lab4/file",
+            "http://127.0.0.1:5000/lab5/function",
             {
-               method: "GET"
+               method: "POST",
+               headers: {
+                  "Content-Type": "application/json",
+               },
+               body: JSON.stringify({ num, value, n, a, b }),
             }
          );
 
@@ -78,8 +87,57 @@ function Lab5(): JSX.Element {
          const y: number[] = data.data_points.map((point: [number, number]) => point[1]);
          setX(x.join(" "));
          setY(y.join(" "));
-         setSolution(data);
-         handleOpen();
+         // setSolution(data);
+         // handleOpen();
+      } catch (error) {
+         console.error(error);
+         handleClose();
+         setErrorText(`Error while processing: ${error}`);
+      }
+   };
+
+   const handleSubmitFile = async (e: React.FormEvent<HTMLFormElement>) => {
+      playSound();
+      console.log("file")
+      e.preventDefault();
+
+      setSolution({
+         err: "",
+         data_points: [],
+         values: [],
+         defy: []
+      });
+
+      const num = numFile
+      const value = VALUE
+      console.log(num, value)
+      try {
+         const response = await fetch(
+            "http://127.0.0.1:5000/lab5/file",
+            {
+               method: "POST",
+               headers: {
+                  "Content-Type": "application/json",
+               },
+               body: JSON.stringify({ num, value }),
+            }
+         );
+
+         const data = await response.json();
+         console.log(data);
+
+         if (data.error) {
+            handleClose();
+            setErrorText(data.error);
+            setError(true);
+            return;
+         }
+         const x: number[] = data.data_points.map((point: [number, number]) => point[0]);
+         const y: number[] = data.data_points.map((point: [number, number]) => point[1]);
+         setX(x.join(" "));
+         setY(y.join(" "));
+         // setSolution(data);
+         // handleOpen();
       } catch (error) {
          console.error(error);
          handleClose();
@@ -94,30 +152,25 @@ function Lab5(): JSX.Element {
 
       setSolution({
          err: "",
-         function: "",
-         coefficients: [],
-         differences: [],
-         epsilon_values: [],
-         pearson_correlation: 0,
-         phi_values: [],
          data_points: [],
-         standard_deviation: 0,
-         func: "",
+         values: [],
+         defy: []
       });
 
       const x = X.trim()?.split(" ").map(Number); // Convert string array to number array
       const y = Y.trim()?.split(" ").map(Number); // Convert string array to number array
-      console.log(JSON.stringify({ x, y }));
+      const value = Number(VALUE)
+      console.log(JSON.stringify({ x, y, value }));
 
       try {
          const response = await fetch(
-            "http://127.0.0.1:5000/lab4/app",
+            "http://127.0.0.1:5000/lab5/app",
             {
                method: "POST",
                headers: {
                   "Content-Type": "application/json",
                },
-               body: JSON.stringify({ x, y }),
+               body: JSON.stringify({ x, y, value }),
             }
          );
 
@@ -130,8 +183,8 @@ function Lab5(): JSX.Element {
             setError(true);
             return;
          }
-         setSolution(data);
-         handleOpen();
+         // setSolution(data);
+         // handleOpen();
       } catch (error) {
          console.error(error);
          handleClose();
@@ -139,50 +192,10 @@ function Lab5(): JSX.Element {
       }
    };
 
-   const handleDownload = async (e: React.FormEvent<HTMLFormElement>) => {
-      playSound();
-      console.log("download")
-      e.preventDefault();
-
-
-      const x = X.trim()?.split(" ").map(Number); // Convert string array to number array
-      const y = Y.trim()?.split(" ").map(Number); // Convert string array to number array
-
-      console.log(JSON.stringify({ x, y, solution }));
-
-      try {
-         const response = await fetch(
-            "http://127.0.0.1:5000/lab4/download",
-            {
-               method: "POST",
-               headers: {
-                  "Content-Type": "application/json",
-               },
-               body: JSON.stringify({ x, y, solution }),
-            }
-         );
-
-         const data = await response.json();
-         console.log(data);
-
-         if (data.error) {
-            setErrorText(data.error);
-            setError(true);
-            return;
-         }
-         setSuccessText(data.success);
-         setSuccess(true);
-
-      } catch (error) {
-         console.error(error);
-         setError(true);
-         setErrorText(`Error while processing: ${error}`);
-      }
-   };
    return (
       <>
          <Container>
-            <header><h1>Lab4 - Approximation</h1></header>
+            <header><h1>Lab5 - Interpolation</h1></header>
             <Box sx={{
                mt: 1,
                background: 'white', padding: '20px', borderColor: 'white',
@@ -224,18 +237,25 @@ function Lab5(): JSX.Element {
                      onChange={(e) => setY(e.target.value)}
                      placeholder='Y'
                   />
+                  <Input
+                     margin="dense"
+                     required
+                     fullWidth
+                     id="value"
+                     name="value"
+                     autoComplete="value"
+                     autoFocus
+                     sx={{ color: 'white' }}
+                     onChange={(e) => setValue(Number(e.target.value))}
+                     placeholder='VALUE'
+                     inputProps={{ min: -100000, max: 100000 }}
+                     type='number'
+                  />
                   <Box sx={{ textAlign: 'center' }}>
                      <Grid container sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
                         <Grid item>
-                           <StyleButton text={"Submit from File"} type={"button"} onclick={(e) => handleSubmitFile(e as any)} />
-                        </Grid>
-                        <Grid item>
                            <StyleButton text={"Submit"} type={"button"} onclick={(e) => handleSubmitString(e as any)} />
                         </Grid>
-                        {isOpen && (<Grid item>
-                           <StyleButton text={"Download"} onclick={(e) => handleDownload(e as any)} type={"button"} />
-                        </Grid>)}
-
                      </Grid>
                   </Box>
                   <Snackbar open={error} autoHideDuration={3000} onClose={() => setError(false)}>
@@ -249,6 +269,139 @@ function Lab5(): JSX.Element {
                      </Alert>
                   </Snackbar>
                </Box>
+
+
+               <Box component="form" noValidate sx={{
+                  mt: 1,
+                  display: 'flex', flexDirection: 'column', alignItems: 'center',
+                  background: 'black', padding: '20px', borderColor: 'white',
+                  borderWidth: '6px', textAlign: 'center', borderStyle: 'solid',
+                  marginTop: '30px', marginBottom: '30px',
+               }}>
+                  <Input
+                     margin="dense"
+                     required
+                     fullWidth
+                     id="numFile"
+                     name="numFile"
+                     autoComplete="numFile"
+                     autoFocus
+                     onChange={(e) => setNumFile(Number(e.target.value))}
+                     sx={{ color: 'white', mb: 1 }}
+                     placeholder='Number of File'
+                     inputProps={{ min: 1, max: 3 }}
+                     type='number'
+                  />
+                  <Input
+                     margin="dense"
+                     required
+                     fullWidth
+                     id="value"
+                     name="value"
+                     autoComplete="value"
+                     autoFocus
+                     sx={{ color: 'white' }}
+                     onChange={(e) => setValue(Number(e.target.value))}
+                     placeholder='VALUE'
+                     inputProps={{ min: -100000, max: 100000 }}
+                     type='number'
+                  />
+                  <Box sx={{ textAlign: 'center' }}>
+                     <Grid container sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <Grid item>
+                           <StyleButton text={"Submit from File"} type={"button"} onclick={(e) => handleSubmitFile(e as any)} />
+                        </Grid>
+                     </Grid>
+                  </Box>
+               </Box>
+
+
+
+               <Box component="form" noValidate sx={{
+                  mt: 1,
+                  display: 'flex', flexDirection: 'column', alignItems: 'center',
+                  background: 'black', padding: '20px', borderColor: 'white',
+                  borderWidth: '6px', textAlign: 'center', borderStyle: 'solid',
+                  marginTop: '30px', marginBottom: '30px',
+               }}>
+                  <Input
+                     margin="dense"
+                     required
+                     fullWidth
+                     id="n"
+                     name="n"
+                     autoComplete="n"
+                     autoFocus
+                     sx={{ color: 'white' }}
+                     onChange={(e) => setN(Number(e.target.value))}
+                     placeholder='N'
+                     inputProps={{ min: -100000, max: 100000 }}
+                     type='number'
+                  />
+                  <Input
+                     margin="dense"
+                     required
+                     fullWidth
+                     id="a"
+                     name="a"
+                     autoComplete="a"
+                     autoFocus
+                     sx={{ color: 'white' }}
+                     onChange={(e) => setA(Number(e.target.value))}
+                     placeholder='A'
+                     inputProps={{ min: -100000, max: 100000 }}
+                     type='number'
+                  />
+                  <Input
+                     margin="dense"
+                     required
+                     fullWidth
+                     id="b"
+                     name="b"
+                     autoComplete="b"
+                     autoFocus
+                     sx={{ color: 'white' }}
+                     onChange={(e) => setB(Number(e.target.value))}
+                     placeholder='B'
+                     inputProps={{ min: -100000, max: 100000 }}
+                     type='number'
+                  />
+                  <Input
+                     margin="dense"
+                     required
+                     fullWidth
+                     id="value"
+                     name="value"
+                     autoComplete="value"
+                     autoFocus
+                     sx={{ color: 'white' }}
+                     onChange={(e) => setValue(Number(e.target.value))}
+                     placeholder='VALUE'
+                     inputProps={{ min: -100000, max: 100000 }}
+                     type='number'
+                  />
+                  <Input
+                     margin="dense"
+                     required
+                     fullWidth
+                     id="numFile"
+                     name="numFile"
+                     autoComplete="numFile"
+                     autoFocus
+                     onChange={(e) => setNumFunc(Number(e.target.value))}
+                     sx={{ color: 'white', mb: 1 }}
+                     placeholder='Number of Function'
+                     inputProps={{ min: 1, max: 2 }}
+                     type='number'
+                  />
+                  <Box sx={{ textAlign: 'center' }}>
+                     <Grid container sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <Grid item>
+                           <StyleButton text={"Submit Function"} type={"button"} onclick={(e) => handleSubmitFunc(e as any)} />
+                        </Grid>
+                     </Grid>
+                  </Box>
+               </Box>
             </Box>
             <Box sx={{
                mt: 1,
@@ -259,10 +412,10 @@ function Lab5(): JSX.Element {
                {(
 
                   <Box sx={{ display: 'flex', alignItems: 'center', overflowX: 'hidden' }}>
-                     <PointTable array={[solution.coefficients,
+                     {/* <PointTable array={[solution.coefficients,
                      [solution.func], [solution.standard_deviation],
                      [solution.pearson_correlation], solution.differences,
-                     solution.phi_values, solution.epsilon_values]} />
+                     solution.phi_values, solution.epsilon_values]} /> */}
                   </Box>
                )}
             </Box>
